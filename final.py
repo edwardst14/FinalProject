054b5b4e525bbc86044d78a946b2b201b68b5296
Tiffanie Edwards <Tiffanieee@TiffaniesMacBot.home>
2020-11-04 15:30:48 -0500

    Minor Changes

--- final.py
+++ final.py
@@ -1,8 +1,7 @@
 #Python Final Project
 #Start Date: 11/02/20
 
-
-import pygame as pg 
+import pygame as pg
 import sys 
 import time 
 from pygame.locals import *
@@ -14,9 +13,15 @@ draw = ""
 XO = "x"
 
 #board details
-width = 600
-height = 600
+width = 800
+height = 800
 board = [0]*3, [0]*3, [0]*3
 
 grayColor = (230, 230, 230)
 blackColor = (0, 0, 0)
+
+#initialize all pygame functionalities
+pg.init()
+
+game_display = pg.display.set_mode((width, height))
+pg.display.update()
