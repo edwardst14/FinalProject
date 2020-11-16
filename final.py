e7de42e1d7fe6c65b74bdaf7b9d047f4ab6c76b2
Tiffanie Edwards <Tiffanieee@TiffaniesMacBot.home>
2020-11-16 01:32:18 -0500

    draw and reset after draw works; need AI

--- final.py
+++ final.py
@@ -1,69 +1,68 @@
 #Python Final Project
 #Start Date: 11/02/20
 
-import pygame as pg
+import pygame 
 import sys 
 import time 
 from pygame.locals import *
+
 
 #GLOABAL VARIABLES
 winner = ""
 XO = "x"
 draw = False
 
-timer = pg.time.Clock()
+timer = pygame.time.Clock()
 
 #board detail variables
 WIDTH = 400
 HEIGHT = 400
 board = [None]*3, [None]*3, [None]*3
 
-gray = (230, 230, 230)
-white = (255, 255, 255)
-black = (0, 0, 0)
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0,200,0)
+orange = (255,127,0)
+gray = (50,50,50)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+bright_orange = (255,215,0)
+
 
 #images 
-x_img = pg.image.load("X2.png")
-o_img = pg.image.load("O5.png")
-TTT = pg.image.load("image2.jpeg")
+x_img = pygame.image.load("X2.png")
+o_img = pygame.image.load("O5.png")
+TTT = pygame.image.load("image2.jpeg")
 #resize images
-x_img = pg.transform.scale(x_img, (100, 100))
-o_img = pg.transform.scale(o_img, (100, 100))
-TTT = pg.transform.scale(TTT, (WIDTH, HEIGHT+100))
+x_img = pygame.transform.scale(x_img, (100, 100))
+o_img = pygame.transform.scale(o_img, (100, 100))
+TTT = pygame.transform.scale(TTT, (WIDTH, HEIGHT+100))
 #########################################################################################
 
 #initialize all pygame functionalities
-pg.init()
+pygame.init()
 fps = 30
 
 #add 100 pixels to display for game status space
-game_display = pg.display.set_mode((WIDTH, HEIGHT+100))
-pg.display.set_caption("Tic-Tac-Toe")
-#########################################################################################
-
-#may delete if not functioning properly
-'''def toggle_fullscreen(game_display):
-    if toggle_fullscreen:
-        game_display = pg.display.set_mode((0, 0), pg.FULLSCREEN)
-    else:
-        game_display = pg.display.set_mode((WIDTH, HEIGHT))'''
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT+100))
+pygame.display.set_caption("Tic-Tac-Toe")
 ##########################################################################################
 def draw_lines():
-    game_display.blit(TTT, (0,0))
-    pg.display.update()
+    gameDisplay.blit(TTT, (0,0))
+    pygame.display.update()
     time.sleep(3)
-    game_display.fill(white)
+    gameDisplay.fill(white)
 
     #vertical lines
-    pg.draw.line(game_display, black, [WIDTH/3,0], [WIDTH/3, HEIGHT], 2)
-    pg.draw.line(game_display, black, [WIDTH/3*2,0], [WIDTH/3*2, HEIGHT], 2)
+    pygame.draw.line(gameDisplay, black, [WIDTH/3,0], [WIDTH/3, HEIGHT], 2)
+    pygame.draw.line(gameDisplay, black, [WIDTH/3*2,0], [WIDTH/3*2, HEIGHT], 2)
 
     #horizontal lines
-    pg.draw.line(game_display, black, [0,HEIGHT/3], [WIDTH, HEIGHT/3], 2)
-    pg.draw.line(game_display, black, [0,HEIGHT/3*2], [WIDTH, HEIGHT/3*2], 2)
+    pygame.draw.line(gameDisplay, black, [0,HEIGHT/3], [WIDTH, HEIGHT/3], 2)
+    pygame.draw.line(gameDisplay, black, [0,HEIGHT/3*2], [WIDTH, HEIGHT/3*2], 2)
     display_status()
 ###############################################################################################
-#needs to be fixed
 def display_status():
     global draw
 
@@ -75,14 +74,14 @@ def display_status():
         message = "It's a Draw!"
 
     #font
-    font = pg.font.Font(None, 30)
+    font = pygame.font.Font(None, 30)
     text = font.render(message, 1, white)
 
     #display message
-    game_display.fill(black, (0, 400, 500, 100))
+    gameDisplay.fill(black, (0, 400, 500, 100))
     text_rect = text.get_rect(center = (WIDTH/2, 500-50))
-    game_display.blit(text, text_rect)
-    pg.display.update()
+    gameDisplay.blit(text, text_rect)
+    pygame.display.update()
 ################################################################################################
 def check_win():
     global board, winner,draw
@@ -92,7 +91,7 @@ def check_win():
             # this row won
             winner = board[row][0]
             #draw winning line 
-            pg.draw.line(game_display, (50,168,74), (0, (row + 1)*HEIGHT/3 -HEIGHT/6),\
+            pygame.draw.line(gameDisplay, (50,168,74), (0, (row + 1)*HEIGHT/3 -HEIGHT/6),\
                               (WIDTH, (row + 1)*HEIGHT/3 - HEIGHT/6 ), 4)
             break
     # check for winning columns
@@ -101,23 +100,22 @@ def check_win():
             # this column won
             winner = board[0][col]
             #draw winning line
-            pg.draw.line(game_display, (50,168,74),((col + 1)* WIDTH/3 - WIDTH/6, 0),\
+            pygame.draw.line(gameDisplay, (50,168,74),((col + 1)* WIDTH/3 - WIDTH/6, 0),\
                           ((col + 1)* WIDTH/3 - WIDTH/6, HEIGHT), 4)
             break
     # check for diagonal winners
     if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] != None):
         # game won diagonally left to right
         winner = board[0][0]
-        pg.draw.line(game_display, (50,168,74), (50, 50), (350, 350), 4)
+        pygame.draw.line(gameDisplay, (50,168,74), (50, 50), (350, 350), 4)
 
     # check 2nd diagonal winner
     if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] != None):
         # game won diagonally right to left
         winner = board[0][2]
-        pg.draw.line (game_display, (50,168,74), (350, 50), (50, 350), 4)
+        pygame.draw.line (gameDisplay, (50,168,74), (350, 50), (50, 350), 4)
 
-    #DRAW DOESNT WORK
-    if(all([all(row) for row in board]) and winner == None ):
+    if(all([all(row) for row in board]) and winner == "" ):
         draw = True
     display_status()
 ############################################################################################################
@@ -148,23 +146,17 @@ def drawXO(row, col):
     board[row-1][col-1] = XO 
     if(XO == 'x'): 
         #display x image in correct position
-        game_display.blit(x_img, (posy, posx)) 
+        gameDisplay.blit(x_img, (posy, posx)) 
         XO = 'o'
     else: 
         #display o image in correct position
-        game_display.blit(o_img, (posy, posx)) 
+        gameDisplay.blit(o_img, (posy, posx)) 
         XO = 'x'
-    pg.display.update() 
+    pygame.display.update() 
 ############################################################################################################
 def user_click(): 
     # get coordinates of mouse click 
-    #pg.event.get() DIDNT HELP
-    x, y = pg.mouse.get_pos() 
-    #print(x,y)
-    '''m1, m2, m3 = pg.mouse.get_pressed()
-    if m1 == 1:
-        x, y = pg.mouse.get_pos()
-        #print(x, y)'''
+    x, y = pygame.mouse.get_pos() 
    
     # get column of mouse click (1-3) 
     if (x < WIDTH / 3): 
@@ -185,7 +177,7 @@ def user_click():
         row = 3 
     else: 
         row = None
-    print(row,col)
+    #print(row,col)
         
     # place image in correct position
     if(row and col and board[row - 1][col - 1] == None): 
@@ -202,35 +194,80 @@ def reset_game():
     draw_lines()
     winner = ""
     board = [[None]*3,[None]*3,[None]*3]
-################################################################################################################
-#handle all the ways of quitting
-def event_handler():
-    for ev in pg.event.get():
-        if ev.type == QUIT or (
-            ev.type == KEYDOWN and (
-                ev.key == K_ESCAPE or
-                ev.key == K_q
-            )):
-            pg.quit()
-            quit()
-        elif ev.type == MOUSEBUTTONDOWN:
-            #activate user_click()
-            user_click()
-            if (winner or draw):
-                reset_game()
-    pg.display.update()
-    timer.tick(fps)
-###########################################################################################
-#toggle_fullscreen(game_display)
+##########################################################################################################
+def game_loop():
+    draw_lines()
+    while True:
+        for ev in pygame.event.get():
+            if ev.type == QUIT or (
+                ev.type == KEYDOWN and (
+                    ev.key == K_ESCAPE or
+                    ev.key == K_q
+                )):
+                pygame.quit()
+                quit()
+            elif ev.type == MOUSEBUTTONDOWN:
+                #activate user_click()
+                user_click()
+                if (winner != "" or draw == True):
+                    reset_game()
+                #print("got here!!")
+        pygame.display.update()
+        timer.tick(fps)
 
-draw_lines() #this doesnt work properly
+def quitgame():
+    pygame.quit()
 
-#main game loop
-while True:
-    event_handler()
+def text_objects(text, font):
+    textSurface = font.render(text, True, (white))
+    return textSurface, textSurface.get_rect()
 
-#Initial Game screen Doesnt Work  
-#DRAW/TIE GAME DOESNT WORK YET
-#GAME DOESNT RESET WHEN THERE'S A DRAW
-#still need AI feature for one player option
-    
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',75)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((WIDTH/2),(HEIGHT/2))
+    game_display.blit(TextSurf, TextRect,)
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+
+        titleText = gameDisplay.blit(x_img, (170, 200))    # title is an image
+        titleText.center = ((WIDTH / 2), (HEIGHT / 2))
+
+        # button(msg, x, y, w, h, inactive, active, action=None)
+        button("PLAY", 50, 350, 98, 40, green, bright_green, game_loop)
+        button("Quit",275,350,98,40,red,bright_red,quitgame)
+
+        pygame.display.update()
+        timer.tick(15)
+#you can call this menu above your game loop.
+
+
+def button(msg, x, y, w, h, inactive, active, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    #print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, active,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, inactive,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+
+
+game_intro()
